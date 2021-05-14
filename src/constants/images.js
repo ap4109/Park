4d25bb882c637ed613936c5529e16728b6750b19
ap4109 <59963013+ap4109@users.noreply.github.com>
2021-05-14 15:25:39 +0530

    Update images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,8 @@
-export const onBoarding1 = require('../assets/images/1.png')
+
 export const onBoarding2 = require('../assets/images/2.png')
 export const onBoarding3 = require('../assets/images/q.png')
 export default {
-    onBoarding1,
+   
     onBoarding2,
     onBoarding3
 }
